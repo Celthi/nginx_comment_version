b589296870922d7e667e1a3c9c84b95f31c89f99
Evan <txtlu0@gmail.com>
2016-05-07 22:31:05 +0800

    read the ngx_queue source codes

--- src/core/ngx_queue.c
+++ src/core/ngx_queue.c
@@ -45,8 +45,10 @@ ngx_queue_middle(ngx_queue_t *queue)
 }
 
 
-/* the stable insertion sort */
-
+/* the stable insertion sort
+* 比如 3->1->2->4 链表
+* 排序后的链表以1->2->3->4->5的形式
+* */
 void
 ngx_queue_sort(ngx_queue_t *queue,
     ngx_int_t (*cmp)(const ngx_queue_t *, const ngx_queue_t *))
@@ -58,7 +60,11 @@ ngx_queue_sort(ngx_queue_t *queue,
     if (q == ngx_queue_last(queue)) {
         return;
     }
-
+    /* 循环不变量为，q之前的链表是有序的，
+     * 初始时链表只有一个元素，所以循环不变量保持
+     * 假设某次循环不变量保持，进入循环语句判断是否循环不变量
+     * 仍然不变。
+     * 终止时。。。*/
     for (q = ngx_queue_next(q); q != ngx_queue_sentinel(queue); q = next) {
 
         prev = ngx_queue_prev(q);
@@ -67,6 +73,8 @@ ngx_queue_sort(ngx_queue_t *queue,
         ngx_queue_remove(q);
 
         do {
+            /* 如果prev 小于等于q则退出do循环
+             * 否则prev 后退一位。*/
             if (cmp(prev, q) <= 0) {
                 break;
             }
@@ -75,6 +83,10 @@ ngx_queue_sort(ngx_queue_t *queue,
 
         } while (prev != ngx_queue_sentinel(queue));
 
+        /* 将q插入prev的后面
+         * insert_after(q, x)等于insert_head(q, x), head中的q表示链表容器，
+         * after中的q表示链表里面的某一个元素。
+         *  */
         ngx_queue_insert_after(prev, q);
     }
 }
